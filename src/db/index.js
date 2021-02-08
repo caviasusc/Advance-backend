b50052e93dd0bfb77f2501e0e6a7e618ffd5d589
Cesar Viasus <caviasusc@gmail.com>
2021-02-08 08:23:54 -0500

    b

--- src/db/index.js
+++ src/db/index.js
@@ -1,11 +1,10 @@
 const {Pool} = require('pg')
 
 const pool = new Pool({
-    host: 'ec2-52-22-161-59.compute-1.amazonaws.com',
-    user: 'dwxqfcunfiwege',
-    password: 'e1c12909d2368c82ad9cb240f2d95d326ce2546b7c7105ee27a2d9fc42816387',
-    database: 'DATABASE',
-    port: process.env.PGPORT
-})
+    connectionString: process.env.DATABASE_URL,
+    ssl: {
+        rejectUnauthorized: false
+    }
+});
 
 module.exports ={pool}
